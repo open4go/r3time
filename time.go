4925c24a57bf94328eb1ea1d84ae752d7a9d1e51
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-03-17 01:13:12 +0800

    add time

---
+++ time.go
@@ -0,0 +1,23 @@
+package r3time
+
+import (
+	"fmt"
+	"time"
+)
+
+// CurrentTime 当前时间
+// 格式 2006.01.02 15:04:05
+func CurrentTime() string {
+	dt := time.Now()
+	t := fmt.Sprintf("%v", dt.Format("2006.01.02 15:04:05"))
+	return t
+}
+
+// Today 获取当天日期
+// 格式 20060102
+func Today() string {
+	// Using time.Now() function.
+	dt := time.Now()
+	t := fmt.Sprintf("%v", dt.Format("20060102"))
+	return t
+}
