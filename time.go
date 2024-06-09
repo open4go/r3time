e9aa3407b7c39e0b5629ff5c851a9f622ee20207
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-10 02:28:45 +0800

    add Timestamp2ReadTime

--- time.go
+++ time.go
@@ -21,3 +21,12 @@ func Today() string {
 	t := fmt.Sprintf("%v", dt.Format("20060102"))
 	return t
 }
+
+// Timestamp2ReadTime 获取可读时间
+// 格式 2006.01.02 15:04:05
+func Timestamp2ReadTime(timestamp int64) string {
+	// Convert timestamp to time.Time
+	dt := time.Unix(timestamp, 0)
+	t := fmt.Sprintf("%v", dt.Format(time.DateTime))
+	return t
+}
